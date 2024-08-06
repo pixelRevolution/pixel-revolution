e415fdad8b8c33af824eab8caf7832b985b0a3d7
pixelRevolution <160487748+pixelRevolution@users.noreply.github.com>
2024-08-06 09:04:03 -0300

    Update jogos.js

--- js/jogos.js
+++ js/jogos.js
@@ -7,7 +7,8 @@ const jogos = [
         descricao: "Um jogo de plataforma 2D side-scroller onde o jogador está em uma nave espacial infestada por vários aliens, referidos como criptomorfos. Quanto mais o jogador avança, mais claustofóbicas as fases ficam e mais tipos de aliens são introduzidos. O jogador iráencontrar diferentes armas durante seu caminho que poderá usar contra os criptomorfos.O jogo terá 10 fases, um diferente da outra. No meio da fase 5, a energia da nave irá cair e asfracas luzes apagam de uma vez. No final, o jogador terá que enfrentar a rainha alien. Após isso, vários jogos irão chocar e o jogador terá que correr em direção à sala de controle, com uma chave. Ao virar a chave, o jogador ativa a sequência de auto-destruição, devendo correr em direção ao módulo de escape. A virada de chave fará tocar um alto som de uma sirene constante. Assim, podefugir, enquanto a nave explode com os aliens dentro.",
         preco: "Grátis",
         genero: "Terror, sobrevivência,Tiro, 2D",
-        tipo: "2D"
+        tipo: "2D",
+        download: ""
     },
     {
         id: "0000003",
@@ -17,7 +18,8 @@ const jogos = [
         descricao: "Cambada do Brasil é um jogo de  hack in Slash de plataforma 2D   ULTRA FRENÉTICO com uma variedade inacreditável de itens poderes e habilidades  juntamente com elementos de RPG e exploração ambientado em uma cidade distópica de Pernambuco chamada Camaragibe. você controla um grupo de mercenários que ganharam Poderes e matam por dinheiro. monstros fantasmas demônios alienígenas e outras coisas bizarras não escapam",
         preco: "Grátis",
         genero: "Exploração",
-        tipo: "2D"
+        tipo: "2D",
+        download: ""
     },
     {
         id: "0000004",
@@ -27,7 +29,8 @@ const jogos = [
         descricao: "Nichoarch se trata de uma noite Perpétua onde você precisa sobreviver em um ambiente escuro e solitário. A sua únicas companhias são frio,chuva, fogueiras e monstros  querendo ceifar a sua vida",
         preco: "Grátis",
         genero: "Sobrevivência",
-        tipo: "2D"
+        tipo: "2D",
+        download: ""
     },
     {
         id: "0000005",
@@ -37,7 +40,8 @@ const jogos = [
         descricao: "Uma guerra civil está acontecendo um dos exércitos está bombardeando violentamente o outro exército, Você faz parte do exército que está sendo bombardeado. Portanto...Se apresse ",
         preco: "Grátis",
         genero: "Guerra, ação e estratégia",
-        tipo: "2D"
+        tipo: "2D",
+        download: ""
     },
     {
         id: "0000002",
@@ -47,7 +51,8 @@ const jogos = [
         descricao: "No jogo dois jogadores se enfrentam, um representando a União Soviética e o outro, os Estados Unidos. Uma partida consiste em ataque, defesa, realização de inteligência, contrainteligência, recebimento de reforços de países aliados e a construção de bases em locais estratégicos. Cada jogador tem 3 linhas para pôr cartas, cada uma com 3 espaços para as cartas. A linha de cima contém as cartas da sua agência de inteligência, a do meio contém cartas viradas para baixo contendo a informação de que cidades bases militares do respectivo país foram construídas. São nessas bases que mísseis balísticos intercontinentais são lançados.",
         preco: "Grátis",
         genero: "Guerra, ação e estratégia",
-        tipo: "Tabuleiro"
+        tipo: "Tabuleiro",
+        download: ""
     }
 ];
 
