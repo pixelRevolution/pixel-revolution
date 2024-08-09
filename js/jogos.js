776d67b0566778a30f12e2a9e3782ac593f13166
pixelRevolution <160487748+pixelRevolution@users.noreply.github.com>
2024-08-09 08:48:18 -0300

    Add files via upload

--- js/jogos.js
+++ js/jogos.js
@@ -2,7 +2,7 @@ const jogos = [
     {
         id: "0000001",
         nome: "Empty Place",
-        image: "../imagens/empty.png",
+        image: "https://i.postimg.cc/TwPj7Fs5/image0-waifu2x-art-noise3.png",
         vercao: "1.0.0",
         descricao: "Um jogo de plataforma 2D side-scroller onde o jogador está em uma nave espacial infestada por vários aliens, referidos como criptomorfos. Quanto mais o jogador avança, mais claustofóbicas as fases ficam e mais tipos de aliens são introduzidos. O jogador iráencontrar diferentes armas durante seu caminho que poderá usar contra os criptomorfos.O jogo terá 10 fases, um diferente da outra. No meio da fase 5, a energia da nave irá cair e asfracas luzes apagam de uma vez. No final, o jogador terá que enfrentar a rainha alien. Após isso, vários jogos irão chocar e o jogador terá que correr em direção à sala de controle, com uma chave. Ao virar a chave, o jogador ativa a sequência de auto-destruição, devendo correr em direção ao módulo de escape. A virada de chave fará tocar um alto som de uma sirene constante. Assim, podefugir, enquanto a nave explode com os aliens dentro.",
         preco: "Grátis",
@@ -13,7 +13,7 @@ const jogos = [
     {
         id: "0000003",
         nome: "Cambada do Brasil",
-        image: "../imagens/cambada.jpg",
+        image: "https://i.postimg.cc/jdgQGhKF/Whats-App-Image-2024-03-27-at-11-12-28.jpg",
         vercao: "1.0.0",
         descricao: "Cambada do Brasil é um jogo de  hack in Slash de plataforma 2D   ULTRA FRENÉTICO com uma variedade inacreditável de itens poderes e habilidades  juntamente com elementos de RPG e exploração ambientado em uma cidade distópica de Pernambuco chamada Camaragibe. você controla um grupo de mercenários que ganharam Poderes e matam por dinheiro. monstros fantasmas demônios alienígenas e outras coisas bizarras não escapam",
         preco: "Grátis",
@@ -24,7 +24,7 @@ const jogos = [
     {
         id: "0000004",
         nome: "Nictoarch",
-        image: "../imagens/nictoarch.jpg",
+        image: "https://i.postimg.cc/7hJSgFJ8/Whats-App-Image-2024-03-27-at-11-12-42.jpg",
         vercao: "1.0.0",
         descricao: "O Nictoarch (estilizado como nictoarch) é um survival horror similar a um RPG com elementos de puzzle e investigação, onde o protagonista se encontra em uma floresta completamente escura, cabendo a ele explorar o ambiente e se proteger de monstros com a luz do fogo. No decorrer da narrativa, o protagonista irá descobrir, através de notas e pesadelos, o motivo de estar ali, se tornando um monstro cuja única razão de existir é causar sofrimento àqueles que vieram depois.",
         preco: "Grátis",
@@ -35,7 +35,7 @@ const jogos = [
     {
         id: "0000005",
         nome: "Hurry Up",
-        image: "../imagens/hurry.jpg",
+        image: "https://i.postimg.cc/tgbp3WfB/Whats-App-Image-2024-03-27-at-11-12-55.jpg",
         vercao: "1.0.0",
         descricao: "Uma guerra civil está acontecendo um dos exércitos está bombardeando violentamente o outro exército, Você faz parte do exército que está sendo bombardeado. Portanto...Se apresse ",
         preco: "Grátis",
@@ -46,7 +46,7 @@ const jogos = [
     {
         id: "0000002",
         nome: "Defcon 0",
-        image: "../imagens/defcon.png",
+        image: "https://i.postimg.cc/25VfBKYC/0-1.png",
         vercao: "1.0.0",
         descricao: "No jogo dois jogadores se enfrentam, um representando a União Soviética e o outro, os Estados Unidos. Uma partida consiste em ataque, defesa, realização de inteligência, contrainteligência, recebimento de reforços de países aliados e a construção de bases em locais estratégicos. Cada jogador tem 3 linhas para pôr cartas, cada uma com 3 espaços para as cartas. A linha de cima contém as cartas da sua agência de inteligência, a do meio contém cartas viradas para baixo contendo a informação de que cidades bases militares do respectivo país foram construídas. São nessas bases que mísseis balísticos intercontinentais são lançados.",
         preco: "Grátis",
